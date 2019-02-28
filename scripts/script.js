b8e85dcc446250bf394461df1343d5139f7d7416
Vasanchai Prakobkij <59070156@it.kmitl.ac.th>
2019-02-28 09:48:54 +0700

    ADD Cursor Glowing

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,21 @@
 
+
+$('document').ready(function(){
+  var prevScrollpos = window.pageYOffset;
+  var navHeight = $(".nav").outerHeight();
+  console.log(navHeight);
+  window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    $(".nav").css({"top":"0"});
+  } else {
+    $(".nav").css({"top":"-"+navHeight+"px"});
+  }
+    prevScrollpos = currentScrollPos;
+  
+}
+});
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
@@ -11,7 +28,7 @@ window.onscroll = function() {
 }
 
 var typed = new Typed('#typed', {
-    strings: ["VASANCHAI"],
+    strings: ["VASANCHAI PRAKOBKIJ"],
     typeSpeed:100
   });
   
@@ -313,3 +330,52 @@ function myFunction() {
     });
   }
 })(jQuery);
+
+var standardbody=(document.compatMode=="CSS1Compat")? document.documentElement : document.body
+
+var nav = (!document.all || window.opera);
+var tmr = null;
+var spd = 50;
+var x = 0;
+var x_offset = -500;
+var y = 0;
+var y_offset = -500;
+
+document.onmousemove = get_mouse; 
+
+function get_mouse(e)
+{    
+  x = (nav) ? e.pageX : event.clientX+standardbody.scrollLeft;
+  y = (nav) ? e.pageY : event.clientY+standardbody.scrollTop;
+  x += x_offset;
+  y += y_offset;
+  beam(1);     
+}
+
+function beam(n)
+{
+  if(n<5)
+  {
+				document.getElementById('div'+n).style.top=y+'px'
+				document.getElementById('div'+n).style.left=x+'px'
+				document.getElementById('div'+n).style.visibility='visible'
+    n++;
+    tmr=setTimeout("beam("+n+")",spd);
+  }
+  else
+  {
+     clearTimeout(tmr);
+     fade(4);
+  }   
+} 
+
+function fade(n)
+{
+  if(n>0) 
+  {
+				document.getElementById('div'+n).style.visibility='hidden'
+    n--;
+    tmr=setTimeout("fade("+n+")",spd);
+  }
+  else clearTimeout(tmr);
+} 
